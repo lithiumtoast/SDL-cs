4d71546c71d174dec349edae5c9c51129f264f42
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2024-04-02 20:55:45 -0400

    Test

--- bindgen/extract.sh
+++ bindgen/extract.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-if ! [[ -x "$(command -v castffi)" ]]; then
-  echo "Error: 'castffi' is not installed. Please visit https://github.com/bottlenoselabs/CAstFfi for instructions to install the CAstFfi tool." >&2
+if ! [[ -x "$(command -v c2ffi)" ]]; then
+  echo "Error: 'c2ffi' is not installed. Please visit https://github.com/bottlenoselabs/c2ffi for instructions to install the tool." >&2
   exit 1
 fi
 
